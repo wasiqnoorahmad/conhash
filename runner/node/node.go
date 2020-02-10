ed5bb84d88480c243e2bf11cd9d62b7a12a07a1a
Wasiq Noor <wasiqnoorahmad@gmail.com>
2020-02-10 15:31:12 +0500

    Adding Cmd parser

--- runner/node/node.go
+++ runner/node/node.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"conhash/node"
+	"flag"
 	"fmt"
+	"strconv"
+)
+
+var (
+	port = flag.Int("--p", 5555, "Port number of node")
+	id   = flag.String("--i", strconv.Itoa(*port), "ID of the node")
+	dst  = flag.String("--d", ":8080", "HostPort of the loadbalancer")
 )
 
 func main() {
-	node := node.New()
-	err := node.StartNode(5555)
+	flag.Parse()
+	node := node.New(*port, *id)
+	err := node.StartNode(*dst)
 
 	if err != nil {
 		fmt.Println("Unable to start Node", err)
