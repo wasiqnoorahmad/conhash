facdc65183c0422516ea7fc3860d59284448c602
Wasiq Noor <wasiqnoorahmad@gmail.com>
2020-02-11 00:22:51 +0500

    Adding automated testcase

--- main.go
+++ main.go
@@ -1,20 +1,61 @@
 package main
 
 import (
-	"conhash/consistent"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
 	"strconv"
 )
 
-func main() {
-	ring := consistent.NewRing()
+// === Cmd Line Args ===
+var (
+	testcase = flag.String("--t", "foo", "Test case id")
+	seed     = 8080
+)
 
-	for i := 0; i < 5; i++ {
-		node := consistent.CNode{
-			Key:    strconv.Itoa(i),
-			Weight: 2,
+func cleanup() {
+	exec.Command("pkill", "--signal", "SIGKILL", "node").Run()
+	exec.Command("pkill", "--signal", "SIGKILL", "lb").Run()
+
+	// os.Remove(".lb.lock")
+}
+
+func waitUntilExist(fpath string) {
+	isExist := false
+	for !isExist {
+		if _, err := os.Stat(fpath); err == nil {
+			isExist = true
 		}
-		hash := ring.GenHash(strconv.Itoa(i))
-		ring.AddNode(hash, &node)
 	}
-	ring.Display()
+}
+
+func startLB() error {
+	cmd := exec.Command("go", "run", "runner/loadbalancer/lb.go", "--p=", strconv.Itoa(seed))
+
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+	waitUntilExist(".lb.lock")
+	go func() {
+		cmd.Wait()
+	}()
+	return nil
+}
+
+func main() {
+
+	cleanup()
+	flag.Parse()
+
+	if err := startLB(); err != nil {
+		fmt.Println("Unable to start LoadBalancer")
+	} else {
+		fmt.Println("Load Balancer started")
+	}
+
+	// time.Sleep(20 * time.Second)
+	cleanup()
+
 }
--- runner/loadbalancer/lb.go
+++ runner/loadbalancer/lb.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"conhash/loadbalancer"
-	"fmt"
+	"flag"
+	"os"
 )
+
+var (
+	port = flag.Int("--p", 8080, "Port number of LoadBalancer")
+)
+
+func createLock() error {
+	f, err := os.Create(".lb.lock")
+	if err != nil {
+		return err
+	}
+	f.Close()
+
+	return nil
+}
 
 func main() {
 	lb := loadbalancer.New()
-	err := lb.StartLB(8080)
+	err := lb.StartLB(*port)
 
 	if err != nil {
-		fmt.Println("Unable to start loadbalancer", err)
+		return
+	}
+
+	if err = createLock(); err != nil {
 		return
 	}
 
